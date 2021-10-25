8f80e02d2a1ec9208a4ad2a25365ff9a9c3bf7a6
ukaymak <unalkaymak69@hotmail.com>
2021-10-25 13:17:33 +0300

    sql init mode changed to never from always rr schema name has been fixed in schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -24,7 +24,7 @@ create table pia.iot_device
     temperature bigint,
     sim_card_fk bigint
         constraint iot_device_sim_card_sim_id_fk
-            references rr.sim_card
+            references pia.sim_card
 );
 
 alter table pia.iot_device owner to postgres;
